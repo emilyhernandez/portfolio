c4c8dfe8f787f00ae463c31d30585a5476cfe280
Emily Hernandez <bulle.96@windowslive.com>
2019-01-12 17:40:20 +0200

    Edit MuiButton + colors + fontsize

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -4,39 +4,46 @@ export default createMuiTheme({
   typography: {
     useNextVariants: true,
     fontFamily: ['Work Sans', 'Oswald'].join(','),
-    fontSize: '18',
+    fontSize: '20',
     h1: {
       textAlign: 'center',
       fontFamily: 'Oswald',
-      fontSize: '5rem',
+      fontSize: '5rem'
     },
     h2: {
       textAlign: 'left',
-      fontFamily: 'Oswald',
+      fontFamily: 'Oswald'
     },
     h3: {},
     h4: {},
     h5: {},
+    body2: { lineHeight: '2rem' }
   },
   palette: {
     type: 'light',
     primary: {
       light: '#F1F1F1',
       main: '#FFFFFF',
-      contrastText: '#292929',
+      contrastText: '#292929'
     },
     secondary: {
       light: '',
-      main: '#CF3141',
+      main: '#ff00e6',
       dark: '',
-      contrastText: '#ffffff',
-    },
+      contrastText: '#ffffff'
+    }
   },
   themeName: 'Ultrahack theme',
   shadows: ['none'],
   overrides: {
     MuiPaper: {},
-    MuiAppBar: {},
-  },
+    MuiButton: {
+      text: {
+        background: 'linear-gradient(45deg, #ff9900, #fd8095)',
+        color: 'white',
+        padding: '10px 30px',
+        borderRadius: '30px'
+      }
+    }
+  }
 });
-
