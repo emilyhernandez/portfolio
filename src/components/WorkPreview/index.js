85c9bc1cddd3739fcdeb4e444ed24fb27f29f246
Emily Hernandez <bulle.96@windowslive.com>
2019-01-13 00:51:42 +0200

    Improve workPreview component

--- src/components/WorkPreview/index.js
+++ src/components/WorkPreview/index.js
@@ -10,28 +10,50 @@ import { compose } from 'redux';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-    root: {
-        border: '5px solid pink',
-        display: 'inline-block',
-        textTransform: 'uppercase',
-        borderImage: 'linear-gradient(to left, #2e98bc 0%, #c66262 100%)',
-        borderImageSlice: '1',
-        borderWidth: '2px',
-        padding: theme.spacing.unit * 2,
-    }
+  root: {
+    width: '100vw',
+    [theme.breakpoints.up('md')]: {
+      width: '44vw'
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: '30vw'
+    },
+    paddingLeft: theme.spacing.unit * 2,
+  },
+  imageDiv: {
+    position: 'relative',
+    paddingTop: '100%'
+  },
+  image: {
+    position: 'absolute',
+    left: '0',
+    top: '0',
+    width: '100%',
+    height: 'auto'
+  }
 });
 
 /* eslint-disable react/prefer-stateless-function */
 export class WorkPreview extends React.Component {
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return <div className={classes.root}>{this.props.children}</div>;
-    }
+    return (
+      <div className={classes.root}>
+        <div className={classes.imageDiv}>
+          <img
+            src={this.props.img}
+            alt=""
+            className={classes.image}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 WorkPreview.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default compose(withStyles(styles))(WorkPreview);
