dca88d818028ff5b20ec13f29eb26379d05a91b8
Emily Hernandez <bulle.96@windowslive.com>
2019-01-12 12:10:16 +0200

    Fix css property name

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { compose } from 'redux'
-import { withStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { compose } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-const contentful = require('contentful')
+const contentful = require('contentful');
 
 const client = contentful.createClient({
   space: 'mmfzlajfgr0g',
   accessToken:
     '4d6df67ab746b1877a1eee63da74946ce10dec59335ac47bd6f2233c2d4a02fe'
-})
+});
 
 const styles = theme => ({
   backgroundImg: {
@@ -24,19 +24,14 @@ const styles = theme => ({
 export class Home extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      img: ''
-    }
   }
-
-  
 
   render() {
     const { classes } = this.props
 
     return (
       <div>
-        <div id="background" className={classes.backgroundImg} />
+        <div className={classes.backgroundImg} />
       </div>
     )
   }
