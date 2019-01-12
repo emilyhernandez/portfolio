efbbb112e7dece287f975879c48b15826bfd3b10
Emily Hernandez <bulle.96@windowslive.com>
2019-01-12 18:20:44 +0200

    Move padding to App + add (fake) routes

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { compose } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import Home from '../home';
 import About from '../about';
 import Header from '../../components/Header/index';
 
-const App = () => (
-  <div id="app">
-    <Header/>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-    </Switch>
-  </div>
-);
+const styles = theme => ({
+  app: { padding: theme.spacing.unit * 2 }
+});
 
-export default App;
+class App extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div id="app" className={classes.app}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Home} />
+          <Route exact path="/cv" component={Home} />
+          <Route exact path="/about" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default compose(withStyles(styles))(App);
