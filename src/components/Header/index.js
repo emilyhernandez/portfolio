be755c61a6b223c41f2bd853e13bfa648220b297
Emily Hernandez <bulle.96@windowslive.com>
2019-01-12 21:32:51 +0200

    Add mobile nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,10 +12,10 @@ import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import './style.css';
 
 const styles = theme => ({
   root: {
@@ -38,13 +38,15 @@ const styles = theme => ({
   },
   menuButton: {
     display: 'block',
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('sm')]: {
       display: 'none'
-    }
+    },
+    transform: 'rotate(0deg)',
+    transition: '.5s ease-in -out'
   },
   sectionDesktop: {
     display: 'none',
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up('sm')]: {
       display: 'block'
     }
   },
@@ -52,15 +54,37 @@ const styles = theme => ({
     '&:hover': {},
     background: 'transparent',
     borderRadius: '0',
-    color: theme.palette.primary.contrastText,
+    color: theme.palette.primary.contrastText
   }
 });
 
 /* eslint-disable react/prefer-stateless-function */
 export class Header extends React.Component {
-  state = {
-    auth: true
-  };
+  state = {};
+
+  openNav() {
+    document.getElementById('myNav').style.height = '100%';
+  }
+
+  closeNav() {
+    document.getElementById('myNav').style.height = '0%';
+  }
+
+  toggleNav() {
+    if (document.getElementById('myNav').style.height == '0%') {
+      document.getElementById('myNav').style.height = '100%';
+
+      document.getElementById('top').classList.toggle('open');
+      document.getElementById('middle').classList.toggle('open');
+      document.getElementById('bottom').classList.toggle('open');
+    } else {
+      document.getElementById('myNav').style.height = '0%';
+
+      document.getElementById('top').classList.toggle('open');
+      document.getElementById('middle').classList.toggle('open');
+      document.getElementById('bottom').classList.toggle('open');
+    }
+  }
 
   render() {
     const { classes } = this.props;
@@ -71,6 +95,7 @@ export class Header extends React.Component {
           <Toolbar className={classes.toolBar}>
             <Typography>Emily Hernandez</Typography>
             <div className={classes.grow} />
+
             <div className={classes.sectionDesktop}>
               <Button className={classes.navButton} component={Link} to="/work">
                 Work
@@ -78,13 +103,36 @@ export class Header extends React.Component {
               <Button className={classes.navButton} component={Link} to="/CV">
                 CV
               </Button>
-              <Button className={classes.navButton} component={Link} to="/about">
+              <Button
+                className={classes.navButton}
+                component={Link}
+                to="/about">
                 About me
               </Button>
             </div>
-            <IconButton className={classes.menuButton} aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
+
+            <div className={classes.menu}>
+              <IconButton className={classes.menuButton}>
+                <div className="con" onClick={this.toggleNav}>
+                  <div id="top" className="bar" />
+                  <div id="middle" className="bar" />
+                  <div id="bottom" className="bar" />
+                </div>
+              </IconButton>
+              <div id="myNav" className="overlay" style={{ height: '0%' }}>
+                <div className="overlay-content">
+                  <Link to="/work" onClick={this.toggleNav}>
+                    Work
+                  </Link>
+                  <Link to="/cv" onClick={this.toggleNav}>
+                    CV
+                  </Link>
+                  <Link to="/about" onClick={this.toggleNav}>
+                    About
+                  </Link>
+                </div>
+              </div>
+            </div>
           </Toolbar>
         </AppBar>
       </div>
