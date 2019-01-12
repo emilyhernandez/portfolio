c8e090c91cfe3ef6bbf902568e5cf7b0ca2b7370
Emily Hernandez <bulle.96@windowslive.com>
2019-01-12 13:03:57 +0200

    Add height and width

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { compose } from 'redux'
-import { withStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { compose } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 /*
 const contentful = require('contentful');
@@ -14,30 +14,33 @@ const client = contentful.createClient({
 */
 
 const styles = theme => ({
+  homePage: {
+    height: '100%',
+    width: '100%',
+  },
   backgroundImg: {
     height: '100%',
-    backgroundImage:
-      'http://www.emilyhrndez.com/img/aeroplane-aircraft-airplane-1465904.jpg',
+    backgroundImage: "url('/img/aeroplane-aircraft-airplane-1465904.jpg')",
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover'
   }
-})
+});
 
 export class Home extends React.Component {
   render() {
-    const { classes } = this.props
+    const { classes } = this.props;
 
     return (
-      <div>
+      <div className={classes.homePage}>
         <div className={classes.backgroundImg} />
       </div>
-    )
+    );
   }
 }
 
 Home.propTypes = {
   classes: PropTypes.object.isRequired
-}
+};
 
-export default compose(withStyles(styles))(Home)
+export default compose(withStyles(styles))(Home);
