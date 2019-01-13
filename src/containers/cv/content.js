d034a2e2fdaadf8f2e29f8013c4a42fc96d45af0
Emily Hernandez <bulle.96@windowslive.com>
2019-01-13 21:35:05 +0200

    Create separate content file

---
+++ src/containers/cv/content.js
@@ -0,0 +1,57 @@
+export const programmingSkills = [
+  { title: 'JavaScript', rating: 3 },
+  { title: 'React.JS', rating: 4 },
+  { title: 'Material-UI', rating: 4 },
+  { title: 'Node.JS', rating: 2 },
+  { title: 'Java', rating: 3 },
+  { title: 'Contentful', rating: 1 },
+  { title: 'HTML5', rating: 5 },
+  { title: 'CSS3', rating: 4 },
+];
+
+export const languageSkills = [
+  { title: 'Finnish', rating: 5 },
+  { title: 'English', rating: 4 },
+  { title: 'Spanish', rating: 3 },
+];
+export const education = [
+  {
+    name: 'Metropolia UAS',
+    title: 'Bachelor of Engineering',
+    startYear: '2016',
+    endYear: '2020',
+    description: 'Majoring in software engineering.',
+  },
+  {
+    name: 'Paimio High School',
+    title: 'Undergraduate',
+    startYear: '2012',
+    endYear: '2016',
+    description: '',
+  },
+  {
+    name: 'Colegio A.M.E.N.',
+    title: 'Exchange year',
+    startYear: '2014',
+    endYear: '2015',
+    description: 'Exchange year in Neuquen, Argentina through Rotary International.',
+  },
+];
+
+export const experience = [
+    {
+        name: 'Metropolia UAS',
+        title: 'Summer internship',
+        startYear: '2017',
+        endYear: '2017',
+        description: 'Majoring in software engineering.',
+    },
+    {
+        name: 'Ultrahack',
+        title: 'Front-end',
+        startYear: '2018',
+        endYear: 'present',
+        description: 'Majoring in software engineering.',
+    },
+
+];
